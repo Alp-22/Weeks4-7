dc12a6a9b99d9f743733eaed587d2007ad699d6c
Alpkurt2 <alpkurt22@hotmail.com>
2025-02-16 21:18:32 -0500

    Added more UI components

--- Assets/ExplorableToy/Scripts/GunSprite.cs
+++ Assets/ExplorableToy/Scripts/GunSprite.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunSprite : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Variables to get sprite values
     int spriteValue = 0;
-    public GameObject bulletSpawner;
-    public GameObject muzzleSpawner;
+    //public GameObject bulletSpawner;
+    //public GameObject muzzleSpawner;
+    public Image image;
     public SpriteRenderer spriteRenderer;
     public Sprite sprite1;
     public Sprite sprite2;
@@ -16,7 +19,7 @@ public class GunSprite : MonoBehaviour
     {
         
     }
-
+    //Gets the dropdown value from the UI
     public void spriteChange(int sprite)
     {
         spriteValue = sprite;
@@ -30,17 +33,26 @@ public class GunSprite : MonoBehaviour
            //transform.localScale = new Vector3(0.567f, 0.567f, 0.567f);
             //bulletSpawner.transform.position = new Vector3(1.58f, 0.68f, 0);
             //muzzleSpawner.transform.position = new Vector3(1.82f, -0.13f, 0);
+
+            //Set the gun sprite and image sprite in UI to the pistol
             spriteRenderer.sprite = sprite1;
+            image.sprite = sprite1;
         }
         if (spriteValue == 1)
         {
             //transform.localScale = Vector3.one;
             //bulletSpawner.transform.position = new Vector3(2.42f, 0.23f, 0);
+
+            //Set the gun sprite and image sprite in UI to the assault rifle
             spriteRenderer.sprite = sprite2;
+            image.sprite = sprite2;
         }
         if (spriteValue == 2)
         {
+
+            //Set the gun sprite and image sprite in UI to the sniper rifle
             spriteRenderer.sprite = sprite3;
+            image.sprite = sprite3;
         }
     }
 }
